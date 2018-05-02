a2b56b1f3aa824541d8c72a0f69364908b07538e
Robert Kleger <robert.kleger@swisscom.com>
2018-05-02 20:20:59 +0200

    💚 fix appveyor build

--- package-scripts.js
+++ package-scripts.js
@@ -4,10 +4,10 @@
 module.exports = {
   scripts: {
     default: 'build',
-    serve: './node_modules/.bin/vue-cli-service serve --open',
-    build: './node_modules/.bin/vue-cli-service build',
-    test: './node_modules/.bin/vue-cli-service test',
-    e2e: './node_modules/.bin/vue-cli-service e2e',
-    lint: './node_modules/.bin/vue-cli-service lint'
+    serve: '.\\..\\node_modules\\.bin\\vue-cli-service serve --open',
+    build: '.\\..\\node_modules\\.bin\\vue-cli-service build',
+    test: '.\\..\\node_modules\\.bin\\vue-cli-service test',
+    e2e: '.\\..\\node_modules\\.bin\\vue-cli-service e2e',
+    lint: '.\\..\\node_modules\\.bin\\vue-cli-service lint'
   }
 };
