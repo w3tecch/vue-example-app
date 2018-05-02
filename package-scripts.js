ec0ae7f9b960d58a328dda78f67e19f9fd0a2168
Robert Kleger <robert.kleger@swisscom.com>
2018-05-02 20:32:20 +0200

    💚 add windows check to package-scripts

--- package-scripts.js
+++ package-scripts.js
@@ -1,13 +1,31 @@
+
+const { ifWindows } = require('nps-utils');
+
 /**
  * Windows: Please do not use trailing comma as windows will fail with token error
  */
 module.exports = {
   scripts: {
     default: 'build',
-    serve: '.\\..\\node_modules\\.bin\\vue-cli-service serve --open',
-    build: '.\\..\\node_modules\\.bin\\vue-cli-service build',
-    test: '.\\..\\node_modules\\.bin\\vue-cli-service test',
-    e2e: '.\\..\\node_modules\\.bin\\vue-cli-service e2e',
-    lint: '.\\..\\node_modules\\.bin\\vue-cli-service lint'
+    serve: ifWindows(
+      '.\\node_modules\\.bin\\vue-cli-service serve --open',
+      './node_modules/.bin/vue-cli-service serve --open'
+    ),
+    build: ifWindows(
+      '.\\node_modules\\.bin\\vue-cli-service build',
+      './node_modules/.bin/vue-cli-service build'
+    ),
+    test: ifWindows(
+      '.\\node_modules\\.bin\\vue-cli-service test',
+      './node_modules/.bin/vue-cli-service test'
+    ),
+    e2e: ifWindows(
+      '.\\node_modules\\.bin\\vue-cli-service e2e',
+      './node_modules/.bin/vue-cli-service e2e',
+    ),
+    lint: ifWindows(
+      '.\\node_modules\\.bin\\vue-cli-service lint',
+      './node_modules/.bin/vue-cli-service lint'
+    )
   }
 };
